4602b5a80428c0ec58fee11cd43763afe4f49520
David Ascanio <daviddeveloper@outlook.com>
2024-10-23 23:09:34 -0500

    project 4life

---
+++ src/app/quienes-somos/page.tsx
@@ -0,0 +1,5 @@
+export default function QuienesSomos(){
+    return (
+        <div className="mt-14 container mx-auto h-[900px] bg-red-500"></div>
+    )
+}
---
+++ src/app/quiero-unirme-al-equipo/page.tsx
@@ -0,0 +1,7 @@
+export default function QuieroUnirmeAlEquipo(){
+    return (
+        <div className="mt-14 container mx-auto h-[900px] bg-red-500">
+            Página para unirse al equipo
+        </div>
+    )
+}
