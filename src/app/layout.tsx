a33df8842170d9a8be70ed5a0f3adecaa68b883b
David Ascanio <daviddeveloper24@outlook.com>
2025-02-03 16:56:30 -0500

    se agregan metas de iconos para compartir

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        
         <link rel="icon" type="image/png" href="images-icons/favicon-96x96.png" sizes="96x96" />
         <link rel="icon" type="image/svg+xml" href="images-icons/favicon.svg" />
         <link rel="shortcut icon" href="images-icons/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="images-icons/apple-touch-icon.png" />
         <link rel="manifest" href="images-icons/site.webmanifest" />
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:url" content="https://www.proteccionimnunitaria.com/"></meta>
+        <meta property="og:title" content="Protección inmunitaria"></meta>
+        <meta property="og:description" content="Descubre nuestra línea exclusiva de productos 4Life, diseñados para fortalecer tu sistema inmunológico y mejorar tu calidad de vida. Vive mejor, siéntete mejor."></meta>
+        <meta property="og:image" content="https://www.proteccionimnunitaria.com/images-icons/favicon.ico"></meta>
+        <meta property="og:image:alt" content="Icono"></meta>
+        <meta property="og:image:type" content="image/jpeg"></meta>
+        <meta property="og:image:width" content="1200"></meta>
+        <meta property="og:image:height" content="630"></meta>
       </head>
       <body className=''>
         <SideBarProvider>
