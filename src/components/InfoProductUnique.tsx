a27be1f7777e62c5abb160c1d6cdb71d367ae465
David Ascanio <daviddeveloper24@outlook.com>
2025-02-11 20:29:15 -0500

    se cambia tamamño de letra

--- src/components/InfoProductUnique.tsx
+++ src/components/InfoProductUnique.tsx
@@ -7,7 +7,7 @@ const InfoProductUnique = () => {
             <div className="flex items-center justify-stretch flex-row py-10 md:px-16 px-4 gap-4 sm:gap-0 w-full">
                 <div className="w-[50%] sm:w-[70%] h-full flex">
                         <div className="w-full sm:w-[80%] gap-6 flex flex-col">
-                            <p className='text-xs sm:text-xl text-center'>Complementa tu dieta y promueve el bienestar general de tu organismo con este suplemento dietario exclusivo de 4Life a base de L-Glutamina y otros aminoácidos que complementan tu dieta y promueven el bienestar de tu organismo</p>
+                            <p className='text-xs sm:text-2xl text-center'>Complementa tu dieta y promueve el bienestar general de tu organismo con este suplemento dietario exclusivo de 4Life a base de L-Glutamina y otros aminoácidos que complementan tu dieta y promueven el bienestar de tu organismo</p>
                             <div className='w-full flex justify-center'>
                                 <button onClick={()=>{window.open("https://colombia.4life.com/davidazul/product/glutamine-prime---colombia/3870", "_blank")}} className=" md:w-[50%] text-xs sm:text-xl min-w-[150px] md:min-w-[220px] bg-blue-800 text-white hover:bg-blue-700 px-3 py-1 rounded-lg font-bold shadow-lg transition-all duration-300 hover:scale-105 hover:from-green-500 hover:to-blue-600"
                                     >
