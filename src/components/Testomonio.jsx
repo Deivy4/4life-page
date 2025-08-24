33027d3ce9d1773e65d6e4b1ddd15d7d88fa31f3
David Ascanio <daviddeveloper24@outlook.com>
2025-08-23 23:29:05 -0500

    testimonio Alfonso AScanio

--- src/components/Testomonio.jsx
+++ src/components/Testomonio.jsx
@@ -4,7 +4,7 @@
 export default function Testimonio() {
   return (
     <section className="container w-full text-gray-700 flex justify-stretch items-center sm:my-10">
-      <div className="flex flex-col justify-center items-center w-full">
+      <div className="flex flex-col justify-center items-center w-full gap-4">
         <h2 className="text-2xl md:text-3xl font-bold mb-6 text-gray-800">
           Nuestros testimonios
         </h2>
@@ -32,6 +32,44 @@ export default function Testimonio() {
                 transformación.
               </p>
               <p className="font-semibold text-gray-900 mt-2">— Azul Lara</p>
+            </div>
+            <div className="flex flex-col items-end w-full">
+              <div className="flex gap-1 text-yellow-500 mt-2">⭐⭐⭐⭐⭐</div>
+              <a
+                href="#seccion-products"
+                className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-xl shadow hover:bg-blue-700 transition"
+              >
+                Conoce más
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="items-stretch max-w-6xl w-full bg-white rounded-2xl shadow-lg p-6 flex flex-col md:flex-row gap-6">
+          {/* Video del testimonio */}
+          <video controls className="w-full md:w-1/3 h-80 rounded-xl shadow-md">
+            <source
+              src="https://res.cloudinary.com/dt4pkrj5j/video/upload/v1756007252/Testimonio-alfonso_dfywcd.mp4"
+              type="video/mp4"
+            />
+            Tu navegador no soporta video.
+          </video>
+
+          {/* Texto del testimonio */}
+          <div className="flex flex-col text-left md:w-2/3 justify-around">
+            <div>
+              <p className="text-lg italic text-gray-700 mb-4">
+                “Después de sufrir un ACV, pensé que mi recuperación sería lenta
+                y limitada. Pero al empezar a tomar Colágeno y Rio Vida, sentí
+                cómo mi cuerpo iba recobrando fuerza y energía día tras día.
+                Este testimonio es la prueba de que, con determinación y los
+                productos adecuados, es posible transformar tu bienestar y
+                recuperar la vitalidad que creías perdida. Espero que mi
+                experiencia inspire a otros a dar el primer paso hacia su propia
+                recuperación y bienestar.”
+              </p>
+              <p className="font-semibold text-gray-900 mt-2">
+                — Alfonso Ascanio
+              </p>
             </div>
             <div className="flex flex-col items-end w-full">
               <div className="flex gap-1 text-yellow-500 mt-2">⭐⭐⭐⭐⭐</div>
